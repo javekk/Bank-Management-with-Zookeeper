744d229b8220aae9a13fc4952f0727b19d237166
javekk <periniraffaele@gmail.com>
2018-11-07 18:20:47 +0100

    add sendMessages class

---
+++ src/eu/upm/adic/SendMessages.java
@@ -0,0 +1,14 @@
+package eu.upm.adic;
+
+
+public interface SendMessages {
+
+    public void sendAdd(Client client, boolean isLeader);
+
+    public void sendUpdate(Client client, boolean isLeader);
+
+    public void sendDelete(Integer accountNumber, boolean isLeader);
+
+    public void sendCreateBank (ClientDB clientDB, boolean isLeader);
+
+}
---
+++ src/eu/upm/adic/SendMessagesBank.java
@@ -0,0 +1,28 @@
+package eu.upm.adic;
+
+
+
+
+public class SendMessagesBank implements SendMessages {
+
+
+    @Override
+    public void sendAdd(Client client, boolean isLeader) {
+
+    }
+
+    @Override
+    public void sendUpdate(Client client, boolean isLeader) {
+
+    }
+
+    @Override
+    public void sendDelete(Integer accountNumber, boolean isLeader) {
+
+    }
+
+    @Override
+    public void sendCreateBank(ClientDB clientDB, boolean isLeader) {
+
+    }
+}
